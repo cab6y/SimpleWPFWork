03ce229e703e476633dd2494895ac715a82ab31b
Cihan Abay <cihan.abay@iksap.com>
2026-02-13 00:26:27 +0300

    categıryappservicetest init

---
+++ SimpleWPFWork.aspnet-core/SimpleWPFWork.Application.Test/Categories/CategoryAppServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWPFWork.Application.Test.Categories
+{
+    internal class CategoryAppServiceTests
+    {
+    }
+}
