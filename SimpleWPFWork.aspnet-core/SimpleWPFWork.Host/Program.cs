ab9883a9c8db9fe490e26ae9598a91f759501e02
Cihan Abay <cihan.abay@iksap.com>
2026-02-14 15:25:35 +0300

    open telemetry ile performans takibi

--- SimpleWPFWork.aspnet-core/SimpleWPFWork.Host/Program.cs
+++ SimpleWPFWork.aspnet-core/SimpleWPFWork.Host/Program.cs
@@ -13,7 +13,27 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseMiddleware<ExceptionHandlerMiddleware>();
+app.Use(async (context, next) =>
+{
+    var sw = System.Diagnostics.Stopwatch.StartNew();
 
+    await next();
+
+    sw.Stop();
+
+    var color = sw.ElapsedMilliseconds switch
+    {
+        < 10 => ConsoleColor.Green,
+        < 50 => ConsoleColor.Yellow,
+        _ => ConsoleColor.Red
+    };
+
+    Console.ForegroundColor = color;
+    Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] " +
+        $"{context.Request.Method,-6} {context.Request.Path,-40} " +
+        $"-> {context.Response.StatusCode} in {sw.ElapsedMilliseconds,4}ms");
+    Console.ResetColor();
+});
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
